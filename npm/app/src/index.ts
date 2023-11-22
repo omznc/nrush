705dbe16ecc26151769691ff4bbc98af88c8af3d
omznc <contact@omarzunic.com>
2023-11-22 01:29:32 +0100

    Set cwd when running. Included READMEs in all builds

--- npm/app/src/index.ts
+++ npm/app/src/index.ts
@@ -34,7 +34,7 @@ function getExePath() {
  */
 function run() {
 	const args = process.argv.slice(2);
-	const processResult = spawnSync(getExePath(), args, { stdio: "inherit" });
+	const processResult = spawnSync(getExePath(), args, { stdio: "inherit", cwd: process.cwd() });
 	process.exit(processResult.status ?? 0);
 }
 
